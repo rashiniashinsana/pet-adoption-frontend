1f6a928c697078376391a546e2ee54dd5fb26a5c
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-02-12 19:02:52 +0530

    Add Dummy Data

--- src/dummydata/AdopterDummyData.ts
+++ src/dummydata/AdopterDummyData.ts
@@ -5,87 +5,31 @@ const AdopterDummyData: Adopter[] = [{
     name: "Rashini Ashinsana",
     email: "rashini@gmail.com",
     phone: "0785643123",
+    address: "Mathara",
+}, {
+    adopter_id: "AD002",
+    name: "Sankha Ravindu",
+    email: "sankha@gmail.com",
+    phone: "07678356755",
+    address: "Galle",
+},{
+    adopter_id: "AD005",
+    name: "Ashan Ravindu",
+    email: "ashan@gmail.com",
+    phone: "0775689011",
     address: "Kandy",
 },{
-
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
+    adopter_id: "AD004",
+    name: "Ruwi Dilshani",
+    email: "ruwi@gmail.com",
+    phone: "0723489756",
     address: "Kandy",
 },{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
+    adopter_id: "AD003",
+    name: "Deshan Vithanage",
+    email: "desh@gmail.com",
+    phone: "0754679073",
     address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-},{
-    adopter_id: "AD001",
-    name: "Rashini Ashinsana",
-    email: "rashini@gmail.com",
-    phone: "0785643123",
-    address: "Kandy",
-
 }];
 
 export default AdopterDummyData;
