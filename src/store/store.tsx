1bc8015fe69e06f74ef1ec96d746685b078c5001
Rashini Vithanage <139869759+rashiniashinsana@users.noreply.github.com>
2025-02-20 23:57:17 +0530

    updated

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,15 +1,21 @@
-import AdopterSlice from "../slice/AdopterSlice.ts";
+
 import PetSlice from "../slice/PetSlice.ts";
 import AdoptionRequestSlice from "../slice/AdoptionRequestSlice.ts";
 import {configureStore} from "@reduxjs/toolkit";
+import UserSlice from "../slice/UserSlice.ts";
+import adopterSlice from "../slice/AdopterSlice.ts";
 
 const store = configureStore({
     reducer: {
-        adopter: AdopterSlice,
+        user: UserSlice,
+        adopter: adopterSlice,
         pet: PetSlice,
         request: AdoptionRequestSlice
 
     },
+    middleware  : (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }),
 });
 
 export default store;
